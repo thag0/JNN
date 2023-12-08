e8685b0b83273e6afcdbe981f0a38e526edbebd1
Thiago <thiagobarroso11@hotmail.com>
2023-12-08 00:57:12 -0300

    ajustes no modelo sequencial

--- rna/modelos/Sequencial.java
+++ rna/modelos/Sequencial.java
@@ -1,27 +1,51 @@
 package rna.modelos;
 
+import rna.avaliacao.perda.ErroMedioQuadrado;
 import rna.avaliacao.perda.Perda;
 import rna.core.Mat;
 import rna.estrutura.Camada;
 import rna.inicializadores.Inicializador;
 import rna.otimizadores.Otimizador;
+import rna.otimizadores.SGD;
 
 /**
  * Modelo básico ainda.
  */
 public class Sequencial{
-   public Camada[] camadas = new Camada[0];
 
+   /**
+    * Lista de camadas do modelo.
+    */
+   public Camada[] camadas;
+
+   /**
+    * Função de perda do modelo.
+    */
+   public Perda perda = new ErroMedioQuadrado();
+   
+   /**
+    * Otimizador do modelo.
+    */
+   public Otimizador otimizador = new SGD();
+
+   /**
+    * Inicializa um modelo sequencial vazio.
+    */
    public Sequencial(){
-
+      this.camadas = new Camada[0];
    }
 
+   /**
+    * Inicializa um modelo sequencial a partir de um conjunto de camadas
+    * definido
+    * @param camadas camadas que serão usadas pelo modelo.
+    */
    public Sequencial(Camada[] camadas){
       this.camadas = camadas;
    }
 
    /**
-    * Adiciona camadas ao modelo.
+    * Adiciona novas camadas ao final da lista de camadas do modelo.
     * @param camada nova camada.
     */
    public void add(Camada camada){
@@ -34,11 +58,95 @@ public class Sequencial{
       this.camadas[this.camadas.length-1] = camada;
    }
 
-   public void inicializar(Inicializador ini){
+   /**
+    * Apaga a última camada contida no modelo.
+    */
+   public void sub(){
+      Camada[] c = this.camadas;
+      this.camadas = new Camada[this.camadas.length-1];
       for(int i = 0; i < this.camadas.length; i++){
-         this.camadas[i].inicializar(ini, ini, 0);
+         this.camadas[i] = c[i];
+      }
+   }
+
+   /**
+    * Configura a função de perda que será utilizada durante o processo
+    * de treinamento da Rede Neural.
+    * @param perda nova função de perda.
+    */
+   public void configurarPerda(Perda perda){
+      if(perda == null){
+         throw new IllegalArgumentException("A função de perda não pode ser nula.");
+      }
+
+      this.perda = perda;
+   }
+
+   /**
+    * Configura o novo otimizador da Rede Neural com base numa nova instância de otimizador.
+    * <p>
+    *    Configurando o otimizador passando diretamente uma nova instância permite configurar
+    *    os hiperparâmetros do otimizador fora dos valores padrão, o que pode ajudar a
+    *    melhorar o desempenho de aprendizado da Rede Neural em cenário específicos.
+    * </p>
+    * Otimizadores disponíveis.
+    * <ol>
+    *    <li> GradientDescent  </li>
+    *    <li> SGD (Gradiente Descendente Estocástico) </li>
+    *    <li> AdaGrad </li>
+    *    <li> RMSProp </li>
+    *    <li> Adam  </li>
+    *    <li> Nadam </li>
+    *    <li> AMSGrad </li>
+    *    <li> Adamax  </li>
+    *    <li> Lion   </li>
+    *    <li> Adadelta </li>
+    * </ol>
+    * <p>
+    *    {@code O otimizador padrão é o SGD}
+    * </p>
+    * @param otimizador novo otimizador.
+    * @throws IllegalArgumentException se o novo otimizador for nulo.
+    */
+   public void configurarOtimizador(Otimizador otimizador){
+      if(otimizador == null){
+         throw new IllegalArgumentException("O novo otimizador não pode ser nulo.");
+      }
+      this.otimizador = otimizador;
+   }
+
+   /**
+    * Inicializa os parâmetros necessários para cada camada do modelo,
+    * além de aleatorizar os kernels e bias.
+    * @param otimizador otimizador usando para ajustar os parâmetros treinavéis do modelo.
+    * @param perda função de perda usada para o treinamento do modelo.
+    * @param iniKernel inicializador para os kernels.
+    */
+   public void compilar(Otimizador otimizador, Perda perda, Inicializador iniKernel){
+      this.compilar(otimizador, perda, iniKernel, null);
+   }
+
+   /**
+    * Inicializa os parâmetros necessários para cada camada do modelo,
+    * além de aleatorizar os kernels e bias.
+    * @param otimizador
+    * @param perda
+    * @param iniKernel inicializador para os kernels.
+    * @param iniBias inicializador para os bias.
+    */
+   public void compilar(Otimizador otimizador, Perda perda, Inicializador iniKernel, Inicializador iniBias){
+      if(iniKernel == null){
+         throw new IllegalArgumentException(
+            "O inicializador para o kernel não pode ser nulo."
+         );
+      }
+
+      for(int i = 0; i < this.camadas.length; i++){
+         this.camadas[i].inicializar(iniKernel, iniBias, 0);
          this.camadas[i].configurarId(i);
       }
+
+      this.otimizador.inicializar(this.camadas);
    }
 
    /**
@@ -52,19 +160,17 @@ public class Sequencial{
       }
    }
 
-   public void treinar(double[][][][] entrada, double[][] saida, int epochs, Perda perda, Otimizador otm, boolean logs){
-      otm.inicializar(this.camadas);
-
+   public void treinar(double[][][][] entrada, double[][] saida, int epochs, boolean logs){
       for(int e = 0; e < epochs; e++){
          double perdaEpoca = 0;
          for(int i = 0; i < entrada.length; i++){
             this.calcularSaida(entrada[i]);
-            perdaEpoca += perda.calcular(this.obterSaida(), saida[i]);
+            perdaEpoca += this.perda.calcular(this.obterSaida(), saida[i]);
 
-            double[] g = perda.derivada(this.obterSaida(), saida[i]);
+            double[] g = this.perda.derivada(this.obterSaida(), saida[i]);
             Mat gradientes = new Mat(g);
             this.calcularGradientes(gradientes);
-            otm.atualizar(this.camadas);
+            this.otimizador.atualizar(this.camadas);
          }
 
          perdaEpoca /= entrada.length;
