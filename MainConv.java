8342e6da1e27e54b8d94bea4a99d03ebe6acb088
Thiago <thiagobarroso11@hotmail.com>
2024-04-09 13:30:46 -0300

    adição de dados de treino do mnist

--- MainConv.java
+++ MainConv.java
@@ -17,7 +17,7 @@ public class MainConv{
 
    static final int NUM_DIGITOS_TREINO = 10;
    static final int NUM_DIGITOS_TESTE  = NUM_DIGITOS_TREINO;
-   static final int NUM_AMOSTRAS_TREINO = 300;
+   static final int NUM_AMOSTRAS_TREINO = 400;
    static final int NUM_AMOSTRAS_TESTE  = 100;
    static final int EPOCAS_TREINO = 10;
 
