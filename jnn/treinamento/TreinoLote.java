118da8705ecc7c63116ecc060f423c928c3a6ad3
Thiago <thiagobarroso11@hotmail.com>
2024-04-27 13:37:01 -0300

    add clone para camadas

--- jnn/treinamento/TreinoLote.java
+++ jnn/treinamento/TreinoLote.java
@@ -86,7 +86,6 @@ class TreinoLote {
 				
 				modelo.zerarGradientes();//zerar gradientes para o acumular pelo lote
 				for (int j = 0; j < entradaLote.length; j++) {
-					double[] saidaAmostra = (double[]) saidaLote[j];
 					modelo.forward(entradaLote[j]);
 
 					if (calcularHistorico) {
