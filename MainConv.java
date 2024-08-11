cc6e48f66169082e26c0391cc3d3fc40bdab4298
Thiago <thiagobarroso11@hotmail.com>
2024-08-11 12:24:18 -0300

    testes

--- MainConv.java
+++ MainConv.java
@@ -36,8 +36,8 @@ public class MainConv {
 	static final int NUM_DIGITOS_TESTE  = NUM_DIGITOS_TREINO;
 	static final int NUM_AMOSTRAS_TREINO = 400;
 	static final int NUM_AMOSTRAS_TESTE  = 100;
-	static final int TREINO_EPOCAS = 10; // += 10min18s - 400 amostras - 10 epocas - 10 lotes
-	static final int TREINO_LOTE = 12;
+	static final int TREINO_EPOCAS = 10; // += 4min53s - 400 amostras - 10 epocas - 10 lotes
+	static final int TREINO_LOTE = 10;
 	static final boolean TREINO_LOGS = true;
 
 	// caminhos de arquivos externos
