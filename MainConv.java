5d90496f25476b3eb0672c8400291aca12a1e2c7
Thiago <thiagobarroso11@hotmail.com>
2025-09-14 23:25:31 -0300

    update mnist test data

--- MainConv.java
+++ MainConv.java
@@ -36,9 +36,9 @@ public class MainConv {
 	static final int NUM_DIGITOS_TREINO = 10;
 	static final int NUM_DIGITOS_TESTE  = NUM_DIGITOS_TREINO;
 	static final int NUM_AMOSTRAS_TREINO = 1_000;
-	static final int NUM_AMOSTRAS_TESTE  = 100;
+	static final int NUM_AMOSTRAS_TESTE  = 500;
 	static final int TREINO_EPOCAS = 12;
-	static final int TREINO_LOTE = 64;
+	static final int TREINO_LOTE = 128;
 	static final boolean TREINO_LOGS = true;
 
 	// caminhos de arquivos externos
