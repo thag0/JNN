ddb21b84994747847b618dbe47a1305475aab9e6
Thiago <thiagobarroso11@hotmail.com>
2025-10-17 20:34:14 -0300

    refatoração do loop de atualização

--- jnn/otimizadores/Lion.java
+++ jnn/otimizadores/Lion.java
@@ -1,45 +1,25 @@
 package jnn.otimizadores;
 
 import jnn.core.tensor.Tensor;
+import jnn.core.tensor.TensorData;
 
 /**
  * <h2>
  *		Lion
  * </h2>
+ * <p>
  *		O otimizador Lion é uma alternativa mais eficientes em
  *		memória que utiliza apenas o sinal de operação para controlar
  *		a magnitude das suas atualizações.
- * <p>
- *    O Lion funciona usando a seguinte expressão:
  * </p>
- * <pre>
- *v -= tA * signum((m * beta1) + (g * (1 - beta2)))
- *m -= (m * beta2) + (g (1 - beta2))
- *</pre>
- * Onde:
- * <p>
- *    {@code v} - variável que será otimizada.
- * </p>
- * <p>
- *    {@code tA} - taxa de aprendizado de otimizador.
- * </p>
- * <p>
- *    {@code m} - coeficientes de momentum do otimizador.
- * </p>
- * <p>
- *    {@code g} - gradiente em relação a variável que será otimizada.
- * </p>
- * <p>
- *    {@code beta1 e beta2} - valores de decaimento dos momentums de primeira
- *    e segunda ordem.
- * </p>
+ * {@link {@code Paper} http://arxiv.org/abs/2302.06675}
  */
 public class Lion extends Otimizador {
 
 	/**
 	 * Valor de taxa de aprendizado padrão do otimizador.
 	 */
-	private static final double PADRAO_TA = 0.001;
+	private static final double PADRAO_LR = 0.001;
 
 	/**
 	 * Valor padrão para o decaimento do momento de primeira ordem.
@@ -54,7 +34,7 @@ public class Lion extends Otimizador {
 	/**
 	 * Valor de taxa de aprendizado do otimizador.
 	 */
-	private final double tA;
+	private final double lr;
 
 	/**
 	 * Decaimento do momentum.
@@ -74,43 +54,43 @@ public class Lion extends Otimizador {
 	/**
 	 * Inicializa uma nova instância de otimizador <strong> Lion </strong> 
 	 * usando os valores de hiperparâmetros fornecidos.
-	 * @param tA taxa de aprendizado do otimizador.
+	 * @param lr taxa de aprendizado do otimizador.
 	 * @param beta1 taxa de decaimento de primeira ordem.
 	 * @param beta2 taxa de decaimento de segunda ordem.
 	 */
-    public Lion(Number tA, Number beta1, Number beta2) {
-		double lr = tA.doubleValue();
-		double b1 = beta1.doubleValue();
-		double b2 = beta2.doubleValue();
+    public Lion(Number lr, Number beta1, Number beta2) {
+		double lr_ = lr.doubleValue();
+		double b1_ = beta1.doubleValue();
+		double b2_ = beta2.doubleValue();
 
-		if (lr <= 0) {
+		if (lr_ <= 0) {
 			throw new IllegalArgumentException(
-				"\nTaxa de aprendizado (" + lr + ") inválida."
+				"\nTaxa de aprendizado (" + lr_ + ") inválida."
 			);
 		}
-		if (b1 <= 0) {
+		if (b1_ <= 0) {
 			throw new IllegalArgumentException(
-				"\nTaxa de decaimento de primeira ordem (" + b1 + ") inválida."
+				"\nTaxa de decaimento de primeira ordem (" + b1_ + ") inválida."
 			);
 		}
-		if (b2 <= 0) {
+		if (b2_ <= 0) {
 			throw new IllegalArgumentException(
-				"\nTaxa de decaimento de segunda ordem (" + b2 + ") inválida."
+				"\nTaxa de decaimento de segunda ordem (" + b2_ + ") inválida."
 			);
 		}
 		
-		this.tA 	 = lr;
-		this.beta1 	 = b1;
-		this.beta2 	 = b2;
+		this.lr 	 = lr_;
+		this.beta1 	 = b1_;
+		this.beta2 	 = b2_;
     }
 
 	/**
 	 * Inicializa uma nova instância de otimizador <strong> Lion </strong> 
 	 * usando os valores de hiperparâmetros fornecidos.
-	 * @param tA taxa de aprendizado do otimizador
+	 * @param lr taxa de aprendizado do otimizador
 	 */
-	public Lion(Number tA) {
-		this(tA, PADRAO_BETA1, PADRAO_BETA2);
+	public Lion(Number lr) {
+		this(lr, PADRAO_BETA1, PADRAO_BETA2);
 	}
 
 	/**
@@ -120,7 +100,7 @@ public class Lion extends Otimizador {
 	 * </p>
 	 */
     public Lion() {
-        this(PADRAO_TA, PADRAO_BETA1, PADRAO_BETA2);
+        this(PADRAO_LR, PADRAO_BETA1, PADRAO_BETA2);
     }
 
     @Override
@@ -138,14 +118,18 @@ public class Lion extends Otimizador {
     public void atualizar() {
 		verificarConstrucao();
 
-        for (int i = 0; i < _params.length; i++) {
-            _params[i].aplicar(_params[i], _grads[i], m[i],
-                (p, g, m) -> p -= tA * Math.signum((m * beta1) + (g * (1.0 - beta1)))
-            );
+		final int n = _params.length;
+        for (int i = 0; i < n; i++) {
+			TensorData p_i = _params[i].data();
+			TensorData g_i = _grads[i].data();
+			TensorData m_i = m[i].data();
 
-            m[i].aplicar(m[i], _grads[i],
-                (m, g) -> m -= (m * beta2) + (g * (1.0 - beta2))
-            );
+			// p -= tA * signum(β1*m + (1-β1)*g)
+			TensorData temp = m_i.clone().mul(beta1).add(g_i, 1.0 - beta1);
+			p_i.add(temp.signum().mul(-lr));
+
+			// m = (β2 * m) + ((1-β2) * g)
+			m_i.mul(beta2).add(g_i, 1.0 - beta2);
         }
     }
 
@@ -153,7 +137,7 @@ public class Lion extends Otimizador {
     public String info() {
         construirInfo();
 
-        addInfo("Lr: " + tA);
+        addInfo("Lr: " + lr);
         addInfo("Beta1: " + beta1);
         addInfo("Beta2: " + beta2);
 
