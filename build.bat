8c0ecce5def6863b42223203650ce4f94fb47aa7
Thiago <thiagobarroso11@hotmail.com>
2025-11-09 17:01:40 -0300

    update build

--- build.bat
+++ build.bat
@@ -1,8 +1,27 @@
-@REM gerar bytecode em /bin 
-@REM depois revisar uma forma mais rápida de compilar tudo
-for /R jnn %%f in (*.java) do (
-    javac -g -parameters -d bin "%%f"
+@echo off
+setlocal enabledelayedexpansion
+
+set SRC_DIR=jnn
+set BIN_DIR=bin
+set JAVA_FILES=
+
+echo Coletando arquivos
+for /R "%SRC_DIR%" %%f in (*.java) do (
+    set JAVA_FILES=!JAVA_FILES! "%%f"
 )
 
-@REM exportar .jar
-jar cvf bin/jnn.jar -C bin .
+javac -g -parameters -d "%BIN_DIR%" -sourcepath "%SRC_DIR%" %JAVA_FILES%
+
+if %errorlevel% neq 0 (
+    echo Erro durante a compilação.
+    exit /b %errorlevel%
+)
+
+echo.
+echo Gerando JAR
+jar cvf "%BIN_DIR%\jnn.jar" -C "%BIN_DIR%" .
+
+echo.
+echo Jar gerado
+
+echo Build finalizado.
