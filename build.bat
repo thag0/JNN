6b2f808d3bbdf2cebdf85625efb378ce4768eca2
Thiago <thiagobarroso11@hotmail.com>
2025-11-09 14:43:44 -0300

    update folders

--- build.bat
+++
@@ -1,8 +0,0 @@
-@REM gerar bytecode em /bin 
-@REM depois revisar uma forma mais rápida de compilar tudo
-for /R jnn %%f in (*.java) do (
-    javac -g -parameters -d bin "%%f"
-)
-
-@REM exportar .jar
-jar cvf bin/jnn.jar -C bin .
