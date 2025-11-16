21ba3a128d1830dd686e3aebbbb3c60712f93644
Thiago <thiagobarroso11@hotmail.com>
2025-11-16 18:48:24 -0300

    update build

--- build.bat
+++ build.bat
@@ -5,6 +5,9 @@ set SRC_DIR=jnn
 set BIN_DIR=bin
 set JAVA_FILES=
 
+if exist "%BIN_DIR%" rmdir /S /Q "%BIN_DIR%"
+mkdir "%BIN_DIR%"
+
 echo Coletando arquivos
 for /R "%SRC_DIR%" %%f in (*.java) do (
     set JAVA_FILES=!JAVA_FILES! "%%f"
@@ -18,12 +21,8 @@ if %errorlevel% neq 0 (
 )
 
 echo.
-echo Gerando JAR
 if exist "%BIN_DIR%\jnn.jar" del "%BIN_DIR%\jnn.jar"
 jar cvf "%BIN_DIR%\jnn.jar" -C "%BIN_DIR%" .
-
-echo.
-echo Jar gerado em "%BIN_DIR%"
 
 @REM echo Gerando Doc 
 @REM javadoc -d "%DOC_DIR%" -sourcepath "." -subpackages jnn
