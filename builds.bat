894ebc56e30f38fbd2d30e02f6dd34dd3c03dc09
Thiago <thiagobarroso11@hotmail.com>
2026-01-09 20:01:45 -0300

    update no script de builds e runs

--- builds.bat
+++ builds.bat
@@ -1,4 +1,7 @@
 @echo off
 
-javac -cp "bin\jnn.jar;lib\ged.jar" -d testes/bin MainConv.java
+@rem -arquivos de testes
+javac -cp "bin\jnn.jar;lib\ged.jar;lib\geim.jar" -d testes/bin Benchmark.java
 javac -cp "bin\jnn.jar;lib\ged.jar;lib\geim.jar" -d testes/bin Conv.java
+
+javac -cp "bin\jnn.jar;lib\ged.jar" -d testes/bin MainConv.java
--- runs.bat
+++ runs.bat
@@ -1,2 +1,3 @@
 @rem java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar" MainConv
 @rem java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;lib\geim.jar" Conv
+@rem java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;lib\geim.jar" Benchmark
