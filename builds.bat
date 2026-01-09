67f14652b45915d43f6171399a74520de1c4cf2b
Thiago <thiagobarroso11@hotmail.com>
2026-01-09 20:29:51 -0300

    testes

--- builds.bat
+++ builds.bat
@@ -1,7 +1,9 @@
 @echo off
 
-@rem -arquivos de testes
-javac -cp "bin\jnn.jar;lib\ged.jar;lib\geim.jar" -d testes/bin Benchmark.java
-javac -cp "bin\jnn.jar;lib\ged.jar;lib\geim.jar" -d testes/bin Conv.java
+@rem - arquivos principais
+@REM javac -cp "bin\jnn.jar;lib\ged.jar" -d testes/bin MainConv.java
 
-javac -cp "bin\jnn.jar;lib\ged.jar" -d testes/bin MainConv.java
+@rem -arquivos de testes
+@REM javac -cp "bin\jnn.jar;lib\ged.jar;lib\geim.jar" -d testes/bin Benchmark.java
+@REM javac -cp "bin\jnn.jar;lib\ged.jar;lib\geim.jar" -d testes/bin Conv.java
+javac -cp "bin\jnn.jar;lib\ged.jar;lib\geim.jar" -d testes/bin TesteJNI.java
--- runs.bat
+++ runs.bat
@@ -1,3 +1,4 @@
 @rem java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar" MainConv
 @rem java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;lib\geim.jar" Conv
 @rem java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;lib\geim.jar" Benchmark
+@rem java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;" TesteJNI
