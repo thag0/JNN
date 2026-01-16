07742718355ab886febd46418779165e89a79992
Thiago <thiagobarroso11@hotmail.com>
2026-01-15 21:35:16 -0300

    reescrita de conv2d backward

--- jnn/nativo/cpu/conv2d.c
+++ jnn/nativo/cpu/conv2d.c
@@ -1,73 +1,6 @@
 #include "conv2d.h"
 
-static inline void corr2d(
-    const double* restrict X, int off_x,
-    const double* restrict K, int off_k,
-    double* restrict DST, int off_dst,
-    int alt_x, int larg_x,
-    int alt_k, int larg_k
-) {
-    const int alt_s  = alt_x  - alt_k  + 1;
-    const int larg_s = larg_x - larg_k + 1;
-
-    for (int i = 0; i < alt_s; i++) {
-        const double* lin_x = X + off_x + i * larg_x;
-        double* lin_dst = DST + off_dst + i * larg_s;
-
-        for (int j = 0; j < larg_s; j++) {
-            double soma = 0.0;
-
-            for (int kh = 0; kh < alt_k; kh++) {
-                const double* px = lin_x + j + kh * larg_x;
-                const double* pk = K + off_k + kh * larg_k;
-
-                #pragma omp simd reduction(+:soma)
-                for (int kw = 0; kw < larg_k; kw++) {
-                    soma += px[kw] * pk[kw];
-                }
-            }
-
-            lin_dst[j] += soma;
-        }
-    }
-
-}
-
-static inline void conv2d_full(
-    const double* restrict X, int off_x,
-    const double* restrict K, int off_k,
-    double* restrict DST, int off_dst,
-    int alt_x, int larg_x,
-    int alt_k, int larg_k
-) {
-    const int alt_s  = alt_x  + alt_k  - 1;
-    const int larg_s = larg_x + larg_k - 1;
-
-    for (int i = 0; i < alt_s; i++) {
-        int kh_ini = i >= alt_x ? i - (alt_x - 1) : 0;
-        int kh_fim = i < alt_k ? i : alt_k - 1;
-
-        for (int j = 0; j < larg_s; j++) {
-            int kw_ini = j >= larg_x ? j - (larg_x - 1) : 0;
-            int kw_fim = j < larg_k ? j : larg_k - 1;
-
-            double soma = 0.0;
-
-            for (int kh = kh_ini; kh <= kh_fim; kh++) {
-                const int lin_x = i - kh;
-                const double* Xp = X + off_x + lin_x * larg_x;
-                const double* Kp = K + off_k + kh * larg_k;
-
-                for (int kw = kw_ini; kw <= kw_fim; kw++) {
-                    soma += Kp[kw] * Xp[j - kw];
-                }
-            }
-
-            DST[off_dst + i * larg_s + j] += soma;
-        }
-    }
-
-}
+// isso aqui ta bom, mas ta dificil de ler já
 
 void cpu_conv2d_forward(const conv2d_fwd_params_t* params) {
     const double* restrict X = params->X;
@@ -149,8 +82,8 @@ void cpu_conv2d_forward(const conv2d_fwd_params_t* params) {
 }
 
 void cpu_conv2d_backward(const conv2d_bwd_params_t* params) {
-    const double *restrict X  = params->X;
-    const double* restrict K  = params->K;
+    const double* restrict X = params->X;
+    const double* restrict K = params->K;
     const double* restrict GS = params->GS;
     double* restrict GK = params->GK;
     double* restrict GE = params->GE;
@@ -162,21 +95,16 @@ void cpu_conv2d_backward(const conv2d_bwd_params_t* params) {
     const int larg_x = params->larg_x;
     const int alt_k = params->alt_k;
     const int larg_k = params->larg_k;
+    
+    const int lotes = params->lotes;
+    const int filtros = params->filtros;
+    const int canais = params->canais;
 
     const int off_x_base = params->off_x;
     const int off_k_base = params->off_k;
     const int off_gb_base = params->off_gb;
     const int off_gs_base = params->off_gs;
     const int off_ge_base = params->off_ge;
-    
-    const int lotes = params->lotes;
-    const int filtros = params->filtros;
-    const int canais = params->canais;
-
-    const int off_x = params->off_x;
-    const int off_k = params->off_k;
-    const int off_gb = params->off_gb;
-    const int off_ge = params->off_ge;
 
     const int alt_s = alt_x - alt_k + 1;
     const int larg_s = larg_x - larg_k + 1;
@@ -185,53 +113,78 @@ void cpu_conv2d_backward(const conv2d_bwd_params_t* params) {
     const int area_k = alt_k * larg_k;
     const int area_gs = alt_s * larg_s;
 
-    #pragma omp parallel for schedule(static) 
+    #pragma omp parallel for schedule(static)
     for (int f = 0; f < filtros; f++) {
-        const int filtro = f;
-
-        const int off_k_f = off_k_base + filtro * canais * area_k;
-        double soma_bias_local = 0;
-
-        for (int l = 0; l < lotes; l++) {
-            const int off_gs = off_gs_base + (l * filtros + f) * area_gs;
-            for (int c = 0; c < canais; c++) {
-                int off_x = off_x_base + (l * canais + c) * area_x;
-                int off_gk = off_k_f + c * area_k;
-                corr2d(
-                    X, off_x,
-                    GS, off_gs,
-                    GK, off_gk,
-                    alt_x, larg_x,
-                    alt_s, larg_s
-                );
-            }
-
-            if (temBias) {
+        if (temBias) {
+            double soma_bias = 0.0;
+            for (int l = 0; l < lotes; l++) {
+                const double* ptr_gs = GS + off_gs_base + (l * filtros + f) * area_gs;
+                #pragma omp simd reduction(+:soma_bias)
                 for (int i = 0; i < area_gs; i++) {
-                    soma_bias_local += GS[off_gs + i];
+                    soma_bias += ptr_gs[i];
+                }
+            }
+            GB[off_gb_base + f] += soma_bias;
+        }
+
+        // grad kernel
+        for (int c = 0; c < canais; c++) {
+            double* base_ptr_gk = GK + off_k_base + (f * canais + c) * area_k;
+
+            for (int kh = 0; kh < alt_k; kh++) {
+                for (int kw = 0; kw < larg_k; kw++) {
+                    double soma = 0.0;
+                    
+                    for (int l = 0; l < lotes; l++) {
+                        const double* ptr_gs = GS + off_gs_base + (l * filtros + f) * area_gs;
+                        const double* ptr_x  = X + off_x_base + (l * canais + c) * area_x;
+                        const double* janela_x = ptr_x + kh * larg_x + kw;
+                        
+                        for (int i = 0; i < alt_s; i++) {
+                            const double* lin_gs = ptr_gs + i * larg_s;
+                            const double* lin_x  = janela_x + i * larg_x;
+
+                            #pragma omp simd reduction(+:soma)
+                            for (int j = 0; j < larg_s; j++) {
+                                soma += lin_gs[j] * lin_x[j];
+                            }
+                        }
+                    }
+
+                    base_ptr_gk[kh * larg_k + kw] += soma;
                 }
             }
         }
-
-        if (temBias) {
-            GB[off_gb_base + f] += soma_bias_local;
-        }
     }
 
+    // grad entrada
     #pragma omp parallel for collapse(2) schedule(static)
     for (int l = 0; l < lotes; l++) {
         for (int c = 0; c < canais; c++) {
-            int off_ge = off_ge_base + (l * canais + c) * area_x;
+            double* ptr_ge = GE + off_ge_base + (l * canais + c) * area_x;
+
             for (int f = 0; f < filtros; f++) {
-                int off_gs = off_gs_base + (l * filtros + f) * area_gs;
-                int off_k  = off_k_base  + (f * canais + c) * area_k;
-                conv2d_full(
-                    GS, off_gs,
-                    K,  off_k,
-                    GE, off_ge,
-                    alt_s, larg_s,
-                    alt_k, larg_k
-                );
+                const double* ptr_gs = GS + off_gs_base + (l * filtros + f) * area_gs;
+                const double* ptr_k  = K + off_k_base + (f * canais + c) * area_k;
+
+                for (int kh = 0; kh < alt_k; kh++) {
+                    for (int kw = 0; kw < larg_k; kw++) {
+                        const double val_k = ptr_k[kh * larg_k + kw];
+                        if (val_k == 0.0) continue;
+                        
+                        double* janela_ge = ptr_ge + kh * larg_x + kw;
+
+                        for (int i = 0; i < alt_s; i++) {
+                            const double* lin_gs = ptr_gs + i * larg_s;
+                            double* lin_ge = janela_ge + i * larg_x;
+
+                            #pragma omp simd
+                            for (int j = 0; j < larg_s; j++) {
+                                lin_ge[j] += lin_gs[j] * val_k;
+                            }
+                        }
+                    }
+                }
             }
         }
     }
--- runs.bat
+++ runs.bat
@@ -4,5 +4,5 @@ set F_NATIVE=--enable-native-access=ALL-UNNAMED
 @REM java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar" MainConv
 @REM java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;lib\geim.jar" Conv
 @rem java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;lib\geim.jar" Benchmark
-@REM java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;" TesteConv
-java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar" TesteJNI
+java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar;" TesteConv
+@REM java --enable-native-access=ALL-UNNAMED -cp "testes\bin;bin\jnn.jar;lib\ged.jar" TesteJNI
