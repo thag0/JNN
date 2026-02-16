d94f686ee1c8576160f653c5df606a77fc900912
Thiago <thiagobarroso11@hotmail.com>
2026-02-16 20:49:21 -0300

    add javadoc

--- jnn/inicializadores/HeUniforme.java
+++ jnn/inicializadores/HeUniforme.java
@@ -3,6 +3,17 @@ package jnn.inicializadores;
 import jnn.core.JNNutils;
 import jnn.core.tensor.Tensor;
 
+/**
+ * Inicializador He Uniforme (Kaiming Uniform).
+ * 
+ * <p>
+ *		Indicado para modelos com funções de ativação ReLU e variações,
+ *		pois mantém a variância das ativações e dos gradientes estável
+ *		ao longo das camadas. Diferente da versão normal (gaussiana),
+ *		os valores são amostrados de uma distribuição uniforme no
+ *		intervalo [-limite, limite], onde limite = sqrt(6 / fanIn).
+ * </p>
+ */
 public class HeUniforme extends Inicializador {
 
     public HeUniforme() {}
